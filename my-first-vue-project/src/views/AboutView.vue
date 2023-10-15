218b24c156436560a888394246949ca51a709a8d
erAlinaAhsan <alina.ahsan13@gmail.com>
2023-10-15 23:21:44 +0530

    Vue 3 section 6

---
+++ my-first-vue-project/src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useFlash } from '@/composables/useFlash'
+
+let { flash } = useFlash()
+</script>
+<template>
+  <div>
+    <p>
+      <button @click="flash('it works')">Click Me</button>
+    </p>
+  </div>
+</template>
---
+++ my-first-vue-project/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useStorage } from '@/composables/useStorage'
+
+let food = useStorage('food', 'tacos')
+let age = useStorage('age', 10)
+
+let obj = useStorage('obj', { one: 'one' })
+</script>
+
+<template>
+  <main>
+    <p>What is your favourite food? <input type="text" v-model="food" /></p>
+    <p>How old are you? <input type="text" v-model="age" /></p>
+  </main>
+</template>
